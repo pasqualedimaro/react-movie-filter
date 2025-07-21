3f2aa0815e6608e8ec3152dee6fd7a49a447ba6d
pasqualedimaro <pakydm15@gmail.com>
2025-07-21 12:34:02 +0200

    initialize react movie project

---
+++ src/data/ListFilm.js
@@ -0,0 +1,8 @@
+const film = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
